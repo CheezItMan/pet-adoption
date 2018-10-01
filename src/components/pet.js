0fdb5ef3d6db15be999429d53cff6c44edcab110
CheezItMan <chris@adadevelopersacademy.org>
2018-09-30 21:56:02 -0700

    converting pet.js to jsx

--- src/components/pet.js
+++ src/components/pet.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.petName),
-    React.createElement("h2", {}, props.petType),
-    React.createElement("h2", {}, props.petBreed)
-  ]);
+  return (
+    <section>
+      <h1>Pet {props.petName}</h1>
+      <h2>{props.petType}</h2>
+      <h2>{props.petBreed}</h2>
+    </section>
+  );
 };
 
 export default Pet;
