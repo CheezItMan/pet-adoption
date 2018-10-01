2ce53aa836aa3e07a483a0676e6526baf7a31f8b
CheezItMan <chris@adadevelopersacademy.org>
2018-09-30 19:40:30 -0700

    added pet Component

--- src/app.js
+++ src/app.js
@@ -1,8 +1,32 @@
 // index.js
 
+const Pet = (props) => {
+    return React.createElement('div', {}, [
+        React.createElement('h1', {}, props.petName), 
+        React.createElement('h2', {}, props.petType),
+        React.createElement('h2', {}, props.petBreed)
+    ]);
+}
+
 const App = () => {
-    return React.createElement("div", {}, 
-            React.createElement('h1', {}, 'Adopt Me!')
+    return React.createElement("div", {}, [
+            React.createElement('h1', {}, 'Adopt Me!'),
+            React.createElement(Pet, {
+                petName: 'Lucky',
+                petType: 'Dog',
+                petBreed: 'Mini-Pincher'
+            }),
+            React.createElement(Pet, {
+                petName: 'Nicky',
+                petType: 'Dog',
+                petBreed: 'Mini-Pincher'
+            }),
+            React.createElement(Pet, {
+                petName: 'Shadow',
+                petType: 'Cat',
+                petBreed: 'Maine Coon'
+            })
+        ]
         );
 }
 
